ff88baeadd9d9bf6a6788faa6e5871416191a7bc
George Koureas <george.s.koureas@gmail.com>
2021-09-22 21:15:51 +0300

    -fixed warning

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactGa from 'react-ga'
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import Hidden from '@material-ui/core/Hidden'
